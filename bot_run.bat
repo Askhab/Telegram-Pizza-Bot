5b6ffc63105c3905a36875652f54bbe92dbf4c07
Askhab Magomedov <asmag86@gmail.com>
2023-03-29 10:51:50 +0300

    initial commit

---
+++ bot_run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call %~dp0/home/shard/Desktop/Projects/Python/Telegram bots/YouTube Python Hub/venv/bin/
+
+cd %~dp0/home/shard/Desktop/Projects/Python/Telegram bots/YouTube Python Hub/
+
+set TOKEN=6178036459:AAEjscom7WQCJKXYZE8TFamHtK-Q3HCmz-M
+
+python bot_telegram.py
+
+pause
